0892e01124f5914d1ce51643691fcbea07825445
sfndiku <sfndiku11@gmail.com>
2025-09-22 20:34:56 +0530

    UI Colours Changed

--- src/QmlControls/QGCPalette.cc
+++ src/QmlControls/QGCPalette.cc
@@ -1,22 +1,22 @@
-/****************************************************************************
- *
- * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
- *
- * QGroundControl is licensed according to the terms in the file
- * COPYING.md in the root of the source code directory.
- *
- ****************************************************************************/
-
+/*  */ /****************************************************************************
+        *
+        * (c) 2009-2024 QGROUNDCONTROL PROJECT <http://www.qgroundcontrol.org>
+        *
+        * QGroundControl is licensed according to the terms in the file
+        * COPYING.md in the root of the source code directory.
+        *
+        ****************************************************************************/
 
 /// @file
 ///     @author Don Gagne <don@thegagnes.com>
 
 #include "QGCPalette.h"
-#include "QGCCorePlugin.h"
 
 #include <QtCore/QDebug>
 
-QList<QGCPalette*>   QGCPalette::_paletteObjects;
+#include "QGCCorePlugin.h"
+
+QList<QGCPalette*> QGCPalette::_paletteObjects;
 
 QGCPalette::Theme QGCPalette::_theme = QGCPalette::Dark;
 
@@ -24,10 +24,7 @@ QMap<int, QMap<int, QMap<QString, QColor>>> QGCPalette::_colorInfoMap;
 
 QStringList QGCPalette::_colors;
 
-QGCPalette::QGCPalette(QObject* parent) :
-    QObject(parent),
-    _colorGroupEnabled(true)
-{
+QGCPalette::QGCPalette(QObject* parent) : QObject(parent), _colorGroupEnabled(true) {
     if (_colorInfoMap.isEmpty()) {
         _buildMap();
     }
@@ -36,84 +33,81 @@ QGCPalette::QGCPalette(QObject* parent) :
     _paletteObjects += this;
 }
 
-QGCPalette::~QGCPalette()
-{
+QGCPalette::~QGCPalette() {
     bool fSuccess = _paletteObjects.removeOne(this);
     if (!fSuccess) {
         qWarning() << "Internal error";
     }
 }
 
-void QGCPalette::_buildMap()
-{
+void QGCPalette::_buildMap() {
+
     //                                      Light                 Dark
     //                                      Disabled   Enabled    Disabled   Enabled
-    DECLARE_QGC_COLOR(window,               "#ffffff", "#ffffff", "#222222", "#222222")
-    DECLARE_QGC_COLOR(windowShadeLight,     "#909090", "#828282", "#707070", "#626262")
-    DECLARE_QGC_COLOR(windowShade,          "#d9d9d9", "#d9d9d9", "#333333", "#333333")
-    DECLARE_QGC_COLOR(windowShadeDark,      "#bdbdbd", "#bdbdbd", "#282828", "#282828")
-    DECLARE_QGC_COLOR(text,                 "#9d9d9d", "#000000", "#707070", "#ffffff")
-    DECLARE_QGC_COLOR(warningText,          "#cc0808", "#cc0808", "#f85761", "#f85761")
-    DECLARE_QGC_COLOR(button,               "#ffffff", "#ffffff", "#707070", "#626270")
-    DECLARE_QGC_COLOR(buttonBorder,         "#ffffff", "#d9d9d9", "#707070", "#adadb8")
-    DECLARE_QGC_COLOR(buttonText,           "#9d9d9d", "#000000", "#A6A6A6", "#ffffff")
-    DECLARE_QGC_COLOR(buttonHighlight,      "#e4e4e4", "#946120", "#3a3a3a", "#fff291")
-    DECLARE_QGC_COLOR(buttonHighlightText,  "#2c2c2c", "#ffffff", "#2c2c2c", "#000000")
-    DECLARE_QGC_COLOR(primaryButton,        "#585858", "#8cb3be", "#585858", "#8cb3be")
-    DECLARE_QGC_COLOR(primaryButtonText,    "#2c2c2c", "#000000", "#2c2c2c", "#000000")
-    DECLARE_QGC_COLOR(textField,            "#ffffff", "#ffffff", "#707070", "#ffffff")
-    DECLARE_QGC_COLOR(textFieldText,        "#808080", "#000000", "#000000", "#000000")
-    DECLARE_QGC_COLOR(mapButton,            "#585858", "#000000", "#585858", "#000000")
-    DECLARE_QGC_COLOR(mapButtonHighlight,   "#585858", "#be781c", "#585858", "#be781c")
-    DECLARE_QGC_COLOR(mapIndicator,         "#585858", "#be781c", "#585858", "#be781c")
-    DECLARE_QGC_COLOR(mapIndicatorChild,    "#585858", "#766043", "#585858", "#766043")
-    DECLARE_QGC_COLOR(colorGreen,           "#008f2d", "#008f2d", "#00e04b", "#00e04b") 
-    DECLARE_QGC_COLOR(colorYellow,          "#a2a200", "#a2a200", "#ffff00", "#ffff00")  
-    DECLARE_QGC_COLOR(colorYellowGreen,     "#799f26", "#799f26", "#9dbe2f", "#9dbe2f")  
-    DECLARE_QGC_COLOR(colorOrange,          "#bf7539", "#bf7539", "#de8500", "#de8500")  
-    DECLARE_QGC_COLOR(colorRed,             "#b52b2b", "#b52b2b", "#f32836", "#f32836")
-    DECLARE_QGC_COLOR(colorGrey,            "#808080", "#808080", "#bfbfbf", "#bfbfbf")
-    DECLARE_QGC_COLOR(colorBlue,            "#1a72ff", "#1a72ff", "#536dff", "#536dff")
-    DECLARE_QGC_COLOR(alertBackground,      "#eecc44", "#eecc44", "#eecc44", "#eecc44")
-    DECLARE_QGC_COLOR(alertBorder,          "#808080", "#808080", "#808080", "#808080")
-    DECLARE_QGC_COLOR(alertText,            "#000000", "#000000", "#000000", "#000000")
-    DECLARE_QGC_COLOR(missionItemEditor,    "#585858", "#dbfef8", "#585858", "#585d83")
-    DECLARE_QGC_COLOR(toolStripHoverColor,  "#585858", "#9D9D9D", "#585858", "#585d83")
-    DECLARE_QGC_COLOR(statusFailedText,     "#9d9d9d", "#000000", "#707070", "#ffffff")
-    DECLARE_QGC_COLOR(statusPassedText,     "#9d9d9d", "#000000", "#707070", "#ffffff")
-    DECLARE_QGC_COLOR(statusPendingText,    "#9d9d9d", "#000000", "#707070", "#ffffff")
-    DECLARE_QGC_COLOR(toolbarBackground,    "#ffffff", "#ffffff", "#222222", "#222222")
-    DECLARE_QGC_COLOR(groupBorder,          "#bbbbbb", "#bbbbbb", "#707070", "#707070")
+    DECLARE_QGC_COLOR(window, "#ffccd5", "#ffccd5", "#590d22", "#590d22")
+    DECLARE_QGC_COLOR(windowShadeLight, "#ffb3c1", "#ffb3c1", "#ffb3c1", "#ffb3c1")
+    DECLARE_QGC_COLOR(windowShade, "#ff8fa3", "#ff8fa3", "#ff8fa3", "#ff8fa3")
+    DECLARE_QGC_COLOR(windowShadeDark, "#ff758f", "#ff758f", "#ff758f", "#ff758f")
+    DECLARE_QGC_COLOR(text, "#a4133c", "#590d22", "#ffb3c1", "#fff0f3")
+    DECLARE_QGC_COLOR(warningText, "#ff4d6d", "#ff4d6d", "#c9184a", "#c9184a")
+    DECLARE_QGC_COLOR(button, "#ffccd5", "#ffccd5", "#a4133c", "#a4133c")
+    DECLARE_QGC_COLOR(buttonBorder, "#ffccd5", "#ffb3c1", "#a4133c", "#ff4d6d")
+    DECLARE_QGC_COLOR(buttonText, "#a4133c", "#590d22", "#ffb3c1", "#fff0f3")
+    DECLARE_QGC_COLOR(buttonHighlight, "#ff8fa3", "#ff4d6d", "#a4133c", "#ff758f")
+    DECLARE_QGC_COLOR(buttonHighlightText, "#590d22", "#fff0f3", "#590d22", "#ffccd5")
+    DECLARE_QGC_COLOR(primaryButton, "#ff4d6d", "#ff758f", "#ff4d6d", "#ff758f")
+    DECLARE_QGC_COLOR(primaryButtonText, "#590d22", "#800f2f", "#590d22", "#800f2f")
+    DECLARE_QGC_COLOR(textField, "#ffccd5", "#ffccd5", "#a4133c", "#ffccd5")
+    DECLARE_QGC_COLOR(textFieldText, "#800f2f", "#590d22", "#590d22", "#590d22")
+    DECLARE_QGC_COLOR(mapButton, "#ff4d6d", "#590d22", "#ff4d6d", "#590d22")
+    DECLARE_QGC_COLOR(mapButtonHighlight, "#ff4d6d", "#c9184a", "#ff4d6d", "#c9184a")
+    DECLARE_QGC_COLOR(mapIndicator, "#ff4d6d", "#c9184a", "#ff4d6d", "#c9184a")
+    DECLARE_QGC_COLOR(mapIndicatorChild, "#ff4d6d", "#a4133c", "#ff4d6d", "#a4133c")
+    DECLARE_QGC_COLOR(colorGreen, "#cherry-blossom-pink", "#cherry-blossom-pink", "#cherry-blossom-pink", "#cherry-blossom-pink")
+    DECLARE_QGC_COLOR(colorYellow, "#pink", "#pink", "#pink", "#pink")
+    DECLARE_QGC_COLOR(colorYellowGreen, "#lavender-blush", "#lavender-blush", "#lavender-blush", "#lavender-blush")
+    DECLARE_QGC_COLOR(colorOrange, "#rose-red", "#rose-red", "#rose-red", "#rose-red")
+    DECLARE_QGC_COLOR(colorRed, "#c9184a", "#c9184a", "#ff4d6d", "#ff4d6d")
+    DECLARE_QGC_COLOR(colorGrey, "#a4133c", "#a4133c", "#ffb3c1", "#ffb3c1")
+    DECLARE_QGC_COLOR(colorBlue, "#amaranth-purple", "#amaranth-purple", "#amaranth-purple", "#amaranth-purple")
+    DECLARE_QGC_COLOR(alertBackground, "#ffb3c1", "#ffb3c1", "#ffb3c1", "#ffb3c1")
+    DECLARE_QGC_COLOR(alertBorder, "#a4133c", "#a4133c", "#a4133c", "#a4133c")
+    DECLARE_QGC_COLOR(alertText, "#590d22", "#590d22", "#fff0f3", "#fff0f3")
+    DECLARE_QGC_COLOR(missionItemEditor, "#ff4d6d", "#ffb3c1", "#ff4d6d", "#ff758f")
+    DECLARE_QGC_COLOR(toolStripHoverColor, "#ff4d6d", "#a4133c", "#ff4d6d", "#ff758f")
+    DECLARE_QGC_COLOR(statusFailedText, "#a4133c", "#590d22", "#ffb3c1", "#fff0f3")
+    DECLARE_QGC_COLOR(statusPassedText, "#a4133c", "#590d22", "#ffb3c1", "#fff0f3")
+    DECLARE_QGC_COLOR(statusPendingText, "#a4133c", "#590d22", "#ffb3c1", "#fff0f3")
+    DECLARE_QGC_COLOR(toolbarBackground, "#ffccd5", "#ffccd5", "#590d22", "#590d22")
+    DECLARE_QGC_COLOR(groupBorder, "#a4133c", "#a4133c", "#590d22", "#590d22")
 
     // Colors not affecting by theming
     //                                              Disabled    Enabled
-    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple,     "#4A2C6D", "#4A2C6D")
-    DECLARE_QGC_NONTHEMED_COLOR(brandingBlue,       "#48D6FF", "#6045c5")
-    DECLARE_QGC_NONTHEMED_COLOR(toolStripFGColor,   "#707070", "#ffffff")
+    DECLARE_QGC_NONTHEMED_COLOR(brandingPurple, "#a4133c", "#a4133c")
+    DECLARE_QGC_NONTHEMED_COLOR(brandingBlue, "#ff4d6d", "#ff758f")
+    DECLARE_QGC_NONTHEMED_COLOR(toolStripFGColor, "#a4133c", "#fff0f3")
 
     // Colors not affecting by theming or enable/disable
-    DECLARE_QGC_SINGLE_COLOR(mapWidgetBorderLight,          "#ffffff")
-    DECLARE_QGC_SINGLE_COLOR(mapWidgetBorderDark,           "#000000")
-    DECLARE_QGC_SINGLE_COLOR(mapMissionTrajectory,          "#be781c")
-    DECLARE_QGC_SINGLE_COLOR(surveyPolygonInterior,         "green")
-    DECLARE_QGC_SINGLE_COLOR(surveyPolygonTerrainCollision, "red")
+    DECLARE_QGC_SINGLE_COLOR(mapWidgetBorderLight, "#fff0f3")
+    DECLARE_QGC_SINGLE_COLOR(mapWidgetBorderDark, "#590d22")
+    DECLARE_QGC_SINGLE_COLOR(mapMissionTrajectory, "#ff4d6d")
+    DECLARE_QGC_SINGLE_COLOR(surveyPolygonInterior, "#ffb3c1")
+    DECLARE_QGC_SINGLE_COLOR(surveyPolygonTerrainCollision, "#c9184a")
 
 // Colors for UTM Adapter
 #ifdef QGC_UTM_ADAPTER
-    DECLARE_QGC_COLOR(switchUTMSP,        "#b0e0e6", "#b0e0e6", "#b0e0e6", "#b0e0e6");
-    DECLARE_QGC_COLOR(sliderUTMSP,        "#9370db", "#9370db", "#9370db", "#9370db");
+    DECLARE_QGC_COLOR(switchUTMSP, "#b0e0e6", "#b0e0e6", "#b0e0e6", "#b0e0e6");
+    DECLARE_QGC_COLOR(sliderUTMSP, "#9370db", "#9370db", "#9370db", "#9370db");
     DECLARE_QGC_COLOR(successNotifyUTMSP, "#3cb371", "#3cb371", "#3cb371", "#3cb371");
 #endif
 }
 
-void QGCPalette::setColorGroupEnabled(bool enabled)
-{
+void QGCPalette::setColorGroupEnabled(bool enabled) {
     _colorGroupEnabled = enabled;
     emit paletteChanged();
 }
 
-void QGCPalette::setGlobalTheme(Theme newTheme)
-{
+void QGCPalette::setGlobalTheme(Theme newTheme) {
     // Mobile build does not have themes
     if (_theme != newTheme) {
         _theme = newTheme;
@@ -121,15 +115,11 @@ void QGCPalette::setGlobalTheme(Theme newTheme)
     }
 }
 
-void QGCPalette::_signalPaletteChangeToAll()
-{
+void QGCPalette::_signalPaletteChangeToAll() {
     // Notify all objects of the new theme
-    for (QGCPalette *palette : std::as_const(_paletteObjects)) {
+    for (QGCPalette* palette : std::as_const(_paletteObjects)) {
         palette->_signalPaletteChanged();
     }
 }
 
-void QGCPalette::_signalPaletteChanged()
-{
-    emit paletteChanged();
-}
+void QGCPalette::_signalPaletteChanged() { emit paletteChanged(); }
